d57e1b89434ce69ad8959a3adf9eea59b09b0763
thanh2k4 <nvthanh12102004@gmail.com>
2024-11-20 03:03:28 +0700

    optimize

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//     moduleFileExtensions: ["js", "jsx", "json", "mjs"],
-//     transform: {
-//         "^.+\\.jsx?$": "babel-jest",
-//     },
-//     transformIgnorePatterns: [
-//         "node_modules/(?!axios)/"
-//     ]
-// };
