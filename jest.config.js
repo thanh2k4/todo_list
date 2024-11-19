cbc60735df5db0751b2d5644093325d2e634b1e9
thanh2k4 <nvthanh12102004@gmail.com>
2024-11-20 02:49:22 +0700

    add babel for ES6

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     moduleFileExtensions: ["js", "jsx", "json", "mjs"],
+//     transform: {
+//         "^.+\\.jsx?$": "babel-jest",
+//     },
+//     transformIgnorePatterns: [
+//         "node_modules/(?!axios)/"
+//     ]
+// };
