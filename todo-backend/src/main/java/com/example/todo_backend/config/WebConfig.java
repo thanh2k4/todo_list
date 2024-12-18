cfd67ed9053ab63f0d7b38cdab43dfc551cbf8e6
thanh2k4 <nvthanh12102004@gmail.com>
2024-12-19 00:56:30 +0700

    cors

--- todo-backend/src/main/java/com/example/todo_backend/config/WebConfig.java
+++ todo-backend/src/main/java/com/example/todo_backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(System.getProperty("CORS_ORIGIN"))
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
