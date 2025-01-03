b91e60ffa2daed9532d158ff1f7161c143ae5b28
thanh2k4 <nvthanh12102004@gmail.com>
2025-01-03 23:36:10 +0700

    fix all

--- todo-backend/src/main/java/com/example/todo_backend/TodoBackendApplication.java
+++ todo-backend/src/main/java/com/example/todo_backend/TodoBackendApplication.java
@@ -3,17 +3,10 @@ package com.example.todo_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class TodoBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("./todo-backend").filename(".env").ignoreIfMissing().load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("CORS_ORIGIN", dotenv.get("CORS_ORIGIN"));
 		SpringApplication.run(TodoBackendApplication.class, args);
 	}
 
